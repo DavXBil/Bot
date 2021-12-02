4d0b7e7e459337b736e1865db6dc84eed62ce6e0
DavidB <david.b@hotmail.fr>
2021-12-02 01:04:34 +0100

    added docstrings

--- Classes/youtube_dl.py
+++ Classes/youtube_dl.py
@@ -54,57 +54,23 @@ class YTDLSource(discord.PCMVolumeTransformer):
         self.duration = self.parse_duration(int(data.get('duration')))
 
 
-    def __str__(self):
-
-        return '**{0.title}** by **{0.uploader}**'.format(self)
-
-
     @classmethod
     async def create_source(
         cls, ctx: commands.Context,
-        search: str, *, loop: asyncio.BaseEventLoop = None
+        search: str, *, loop: asyncio.AbstractEventLoop
         ):
-
-        loop = loop or asyncio.get_event_loop()
+        """extract info from url"""
 
         partial = functools.partial(cls.ytdl.extract_info, search, download=False, process=False)
         data = await loop.run_in_executor(None, partial)
 
-        if data is None:
-            raise YTDLError('Couldn\'t find anything that matches `{}`'.format(search))
-
-        if 'entries' not in data:
-            process_info = data
-        else:
-            process_info = None
-            for entry in data['entries']:
-                if entry:
-                    process_info = entry
-                    break
-
-            if process_info is None:
-                raise YTDLError('Couldn\'t find anything that matches `{}`'.format(search))
-
-        webpage_url = process_info['webpage_url']
+        webpage_url = data['webpage_url']
         partial = functools.partial(cls.ytdl.extract_info, webpage_url, download=False)
         processed_info = await loop.run_in_executor(None, partial)
 
-        if processed_info is None:
-            raise YTDLError('Couldn\'t fetch `{}`'.format(webpage_url))
+        return cls(ctx, discord.FFmpegPCMAudio(processed_info['url'], **cls.FFMPEG_OPTIONS),
+            data = processed_info)
 
-        if 'entries' not in processed_info:
-            info = processed_info
-        else:
-            info = None
-            while info is None:
-                try:
-                    info = processed_info['entries'].pop(0)
-                except IndexError:
-                    raise YTDLError(
-                        'Couldn\'t retrieve any matches for `{}`'.format(webpage_url)
-                        ) from None
-
-        return cls(ctx, discord.FFmpegPCMAudio(info['url'], **cls.FFMPEG_OPTIONS), data=info)
 
     def parse_duration(self, duration: int):
         """parses song's duration time"""
